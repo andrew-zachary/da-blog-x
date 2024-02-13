c7c1064bd433bc351a3f0294713b55b43c00e178
andrew-zachary <andrew_zachary@outlook.com>
2024-02-13 00:00:00 +0000

    create posts-controller and render posts-page

--- start/routes.ts
+++ start/routes.ts
@@ -7,6 +7,8 @@
 |
 */
 
+import PostsController from '#controllers/posts_controller'
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
+router.get('/posts', [PostsController, 'index'])
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,22 @@ export default {
       secondary: {
         DEFAULT: 'var(--secondary)'
       },
+      tertiary: {
+        DEFAULT: 'var(--tertiary)'
+      },
       black: 'var(--black)',
       white: 'var(--white)',
       transparent: 'var(--transparent)',
       extend: {
       },
+    },
+    screens: {
+      xs: '320px',
+      sm: '475px',
+      md: '768px',
+      lg: '992px',
+      xlg: '1200px',
+      '2xl': '1440px'
     }
   },
   plugins: [],
